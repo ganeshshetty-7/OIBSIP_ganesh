937bd7249fabab03a69564646125de193afb03a4
ganeshshetty-7 <ganeshshetty802@gmail.com>
2025-04-27 19:57:16 +0530

    adding changes to files

--- login_authentication/app.py
+++ login_authentication/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash  
 
 app = Flask(__name__)
 
